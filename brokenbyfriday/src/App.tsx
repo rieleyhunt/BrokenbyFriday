f0eaf69d853cd50549457b00b9ab5ac7649d5e64
Rieley <huntrieley@gmail.com>
2025-03-20 16:58:37 -0400

    Working on the front end in App.tsx. Currently getting the main page setup from the reference design in Figma

--- brokenbyfriday/src/App.tsx
+++ brokenbyfriday/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import bbfLogo from './assets/bbf.png';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="app-container">
+      {/*Add the logo*/}
+      <img src={bbfLogo} alt = "Broken By Friday Logo" className = "logo" />
+      {/*Add the band name and description*/}
+      <h1 className="we-are-broken-by-friday">We are Broken By Friday...</h1>
+      <p className="ottawa-pop-punk">Ottawa’s up and coming pop punk sensation</p>
+      <p className="broken-by-friday">Broken By Friday ©</p>
+      <p className="upcoming">Upcoming</p>
+      <p className="gallery">Gallery</p>
+      <p className="booking">Booking</p>
+      <p className="home">Home</p>
+    </div>
+  );
+};
 
-export default App
+export default App;
