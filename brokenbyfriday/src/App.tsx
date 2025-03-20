06655f47f5c708478a20dc6eb4ddca07e5c5b0e5
Rieley <huntrieley@gmail.com>
2025-03-20 18:16:12 -0400

    Made huge progress on the front end. Used relative positioning for the web app to adapt to all kinds of displays

--- brokenbyfriday/src/App.tsx
+++ brokenbyfriday/src/App.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import bbfLogo from './assets/bbf.png';
+import Concert1 from './assets/Concert1.jpg';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="app-container">
       {/*Add the logo*/}
-      <img src={bbfLogo} alt = "Broken By Friday Logo" className = "logo" />
-      {/*Add the band name and description*/}
-      <h1 className="we-are-broken-by-friday">We are Broken By Friday...</h1>
-      <p className="ottawa-pop-punk">Ottawa’s up and coming pop punk sensation</p>
-      <p className="broken-by-friday">Broken By Friday ©</p>
-      <p className="upcoming">Upcoming</p>
-      <p className="gallery">Gallery</p>
-      <p className="booking">Booking</p>
-      <p className="home">Home</p>
+      <div className="logo-container">
+        <img src={Concert1} alt= "Concert1" className = "concert1" />
+        <img src={bbfLogo} alt = "Broken By Friday Logo" className = "logo" />
+      </div>
+      <div className="buttons-container">
+          <button type="button" className="button">Home</button>
+          <button type="button" className="button">Upcoming</button>
+          <button type="button" className="button">Gallery</button>
+          <button type="button" className="button">Booking</button>
+        </div>
+      <div className="main-page">
+        {/*Add the band name and description */}
+        <h1 className="we-are-broken-by-friday">We are Broken By Friday...</h1>
+        <p className="ottawa-pop-punk">Ottawa’s up and coming pop punk sensation</p>
+        <p className="broken-by-friday">Broken By Friday ©</p>
+      </div>
     </div>
   );
 };
